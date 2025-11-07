572faf5c9b94550915ef3ddcab269c1f8429817f
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-07 09:27:27 +0700

    membuat SplashScreen2 dan SplashScreen3

---
+++ lib/screen/SplashScreen2.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen2 extends StatelessWidget {
+  const Splashscreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 180,
+              height: 180,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                // color: Colors.amber,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/image2.jpg"),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Start Something New",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "“Every great idea starts with a single tap.”",
+              style: TextStyle(
+                fontSize: 10.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 5,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+                SizedBox(width: 5),
+                Container(
+                  width: 5,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+                SizedBox(width: 5),
+                Container(
+                  width: 5,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 100),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 2,
+              ),
+              child: const Text("Continue", style: TextStyle(fontSize: 12.0)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
