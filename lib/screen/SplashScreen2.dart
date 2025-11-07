899cbfaf8b7f3b9f8ef339440a779f552283ad07
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-07 09:40:32 +0700

    mengfungsikan tombol Continue pada SplashScreen2 seperti SplashScreen1

--- lib/screen/SplashScreen2.dart
+++ lib/screen/SplashScreen2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_uts/screen/SplashScreen3.dart';
 
 class Splashscreen2 extends StatelessWidget {
   const Splashscreen2({super.key});
@@ -75,7 +76,12 @@ class Splashscreen2 extends StatelessWidget {
                 height: 38,
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Splashscreen3()),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
                     foregroundColor: Colors.white,
