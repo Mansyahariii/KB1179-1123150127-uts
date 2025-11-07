5403809c293e54483d22dc914e633217787e148d
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-07 12:38:00 +0700

    menghapus tombol OTP dan menjadikan tombol Continue responsive

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -14,104 +14,102 @@ class Loginscreen extends StatelessWidget {
             Text(
               "Login",
               style: TextStyle(
-                fontFamily: 'Poppins',
                 fontSize: 50.0,
                 fontWeight: FontWeight.w800,
                 color: Colors.white,
               ),
             ),
-            Text(
-              "Phone Number",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Colors.white70,
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Enter Number',
-                hintStyle: TextStyle(
-                  color: const Color.fromARGB(90, 255, 255, 255),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white54, width: 2),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2),
-                ),
-              ),
-            ),
-            Text(
-              "Password",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Colors.white70,
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Enter Password',
-                hintStyle: TextStyle(
-                  color: const Color.fromARGB(90, 255, 255, 255),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white54, width: 2),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2),
-                ),
-                suffixIcon: Container(
-                  margin: EdgeInsets.only(right: 15),
-                  child: const Icon(
-                    Icons.visibility_off,
-                    size: 20,
-                    color: Colors.white,
+            SizedBox(height: 70),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Phone Number",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
                   ),
                 ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green[900],
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Enter Number',
+                    hintStyle: TextStyle(
+                      color: const Color.fromARGB(90, 255, 255, 255),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white54, width: 2),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                  ),
                 ),
-                // elevation: 2,
-              ),
-              child: const Icon(Icons.fingerprint),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 21, 10, 22),
-                foregroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                SizedBox(height: 20),
+                Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
                 ),
-                elevation: 2,
-              ),
-              child: const Text(
-                "Continue",
-                style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                TextField(
+                  obscureText: true,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    hintStyle: TextStyle(
+                      color: const Color.fromARGB(90, 255, 255, 255),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white54, width: 2),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                    suffixIcon: Container(
+                      margin: EdgeInsets.only(right: 15),
+                      child: const Icon(
+                        Icons.visibility_off,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 21, 10, 22),
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  elevation: 2,
+                ),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
